60a114a1a89be44c70af21e73931309406a2138e
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-01-15 07:45:30 +0000

    次は削除機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
 
 
   def index
+    @books = Book.all
   end
 
   def show
@@ -18,7 +19,16 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  #destroyのアクション記述
 
   private
   def book_params
