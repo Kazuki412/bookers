a1e6001fbbe724daef5e6f71d77c22c133670e81
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-01-15 16:24:30 +0000

    次はフラッシュメッセージだよ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,10 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-       redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to book_path(@book.id)
     else
-      @book = Book.new
       render :new
     end
   end
@@ -27,9 +26,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book.id)
+    else
+       render :edit
+    end
   end
 
   def destroy
